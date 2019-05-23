1b13db2657187298e31028b01b22ff7e86c1d4a4
Helio Rocha <hjrocha@gmail.com>
2019-05-23 09:36:08 +0100

    first commit

---
+++ ClassLibrary2/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2
+{
+    public class Class1
+    {
+        public bool IsPrime(int candidate)
+        {
+            if (candidate == 1)
+            {
+                return false;
+            }
+            throw new NotImplementedException("Please create a test first");
+        }
+
+        public bool IsEven(int candidate)
+        {
+            return candidate % 2 == 0;
+        }
+
+        public bool IsOdd(int candidate)
+        {
+            return candidate % 2 == 1;
+        }
+
+        public int AddANumberToSeven(int number)
+        {
+            return number + 7;
+        }
+
+        public string GenerateString(int length)
+        {
+            return new string('x', length);
+        }
+
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using ClassLibrary2;
+using System;
+using Xunit;
+
+namespace UnitTestProject1
+{
+
+    public class UnitTest1
+    {
+
+        private readonly Class1 _primeService;
+
+        public UnitTest1()
+        {
+            _primeService = new Class1();
+        }
+
+        [Fact]
+        public void ReturnFalseGivenValueOf1()
+        {
+            var result = _primeService.IsPrime(1);
+
+            Assert.False(result, "1 should not be prime");
+        }
+
+        [Fact]
+        public void Test01()
+        {
+            int n = _primeService.AddANumberToSeven(5);
+            Assert.Equal(n, 5 + 7);
+        }
+
+
+        [Fact]
+        public void Test02()
+        {
+            string test = _primeService.GenerateString(10);
+            Assert.Equal(test.Length, 10);
+        }
+    }
+}
